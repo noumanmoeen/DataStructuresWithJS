6c823a114308887768164ae4e8bbaae33ed5a196
noumanmoeen <moeennouman7@gmail.com>
2022-12-29 23:39:23 +0500

    Added bubble sort

--- Sorting/BubbleSort.js
+++ Sorting/BubbleSort.js
@@ -1,1 +1,19 @@
-console.log('Hello')
+const BubbleSort = (array) => {
+  let temp;
+  for (let index = array.length; index > 0; index--) {
+    for (let i = 0; i < index; i++) {
+      if (array[i] > array[i + 1]) {
+        temp = array[i + 1];
+        array[i + 1] = array[i];
+        array[i] = temp;
+      }
+    }
+  }
+  return array;
+};
+
+let sampleArray = [4, 65, 7, 2, 76, 2, 7, 21];
+
+const bubbleSortedArray = BubbleSort(sampleArray);
+
+console.log(bubbleSortedArray);
