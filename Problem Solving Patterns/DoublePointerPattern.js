d93c9e6afe39677f4f4f2cedfc874d594953a847
moeennouman7@gmail.com <moeennouman7@gmail.com>
2023-03-07 00:55:19 +0500

    Added sliding widnow pattern

---
+++ Problem Solving Patterns/DoublePointerPattern.js
@@ -0,0 +1,7 @@
+// Write a function to count unique values in an array
+// Arrays will always be sorted
+// Examples
+// [1,1,1,1,1,1,2] ====> 2
+// [1,2,3,4,5,6] ====> 6
+// [1,1,1,1,1,1,] ====> 1
+// [] ====> 0
---
+++ Problem Solving Patterns/SlidingWindowPattern.js
@@ -0,0 +1,26 @@
+// Write a function called maxSubArray which acepts an array of integrs and a number n. The function should calculate the maximum sum if n consecutive numbers in the given array
+// maxSubArray([1,2,5,2,8,1,5],1) ----> 10
+// maxSubArray([4.2,1,6],1) ----> 6
+// maxSubArray([],1) ----> null
+
+const maxSubArray = (arr, n) => {
+  let sum = 0;
+  let temp = 0;
+  for (let i = 0, b = n - 1; i < arr.length; i++, b++) {
+    console.log("herere ", i, b);
+    if (i === b) {
+      if (temp > sum) {
+        sum = temp;
+      }
+    }
+    temp = temp + arr[i];
+  }
+  return sum;
+};
+
+console.log(maxSubArray([1, 2, 5, 2, 8, 1, 5], 3));
+// console.log(maxSubArray([4.2, 1, 6], 1));
+// console.log(maxSubArray([1, 2, 5, 2, 8, 1, 5], 4));
+
+
+ 
