64277df87b15bd14fd900a2b1f753d1e1f481b74
moeennouman7@gmail.com <moeennouman7@gmail.com>
2023-03-19 17:32:37 +0500

    Added selcttion sort

---
+++ Sorting/SelectionSort.js
@@ -0,0 +1,24 @@
+const selectionSort = (array) => {
+  let min = 0;
+  let index = 0;
+  let temp = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    min = array[i];
+    index = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (min > array[j]) {
+        min = array[j];
+        index = j;
+      }
+    }
+    temp = array[i];
+    array[i] = min;
+    array[index] = temp;
+  }
+  return array;
+};
+
+const testArr = [5, 3, 4, 1, 2];
+
+console.log(selectionSort(testArr));
