9fbd8ee0d067c42e391bb5d4f9dad8dac1a5bc05
moeennouman7@gmail.com <moeennouman7@gmail.com>
2024-01-02 00:22:19 +0500

    Soved sumZero problem

--- Problem Solving Patterns/DoublePointerPattern.js
+++ Problem Solving Patterns/DoublePointerPattern.js
@@ -5,3 +5,34 @@
 // [1,2,3,4,5,6] ====> 6
 // [1,1,1,1,1,1,] ====> 1
 // [] ====> 0
+
+// PROBLEM STATEMENT
+//
+// Write a function "sumZero" that takes a sorted array and return the first two values whose sum is equal to zero
+
+const sumZero = (arr) => {
+  if (arr.length < 2) {
+    return [];
+  }
+
+  let left = 0;
+  let right = arr.length - 1;
+  for (let index = 0; index < arr.length; index++) {
+    if (left === right) {
+      return [];
+    }
+
+    if (arr[left] + arr[right] === 0) {
+      return [arr[left], arr[right]];
+    }
+    left++;
+    right--;
+  }
+
+  return [];
+};
+
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]));
+console.log(sumZero([-2, -1, 0, 1, 3]));
+console.log(sumZero([1, 2, 3]));
+console.log(sumZero([3]));
