f1f5e097024061fc7eda37eeaaa7d9b41e00f3e2
moeennouman7@gmail.com <moeennouman7@gmail.com>
2024-01-02 23:19:14 +0500

    Solved sliding window pattern problem and completed problem solving patterns

--- Problem Solving Patterns/DoublePointerPattern.js
+++ Problem Solving Patterns/DoublePointerPattern.js
@@ -1,3 +1,6 @@
+
+// PROBLEM STATEMENT
+
 // Write a function to count unique values in an array
 // Arrays will always be sorted
 // Examples
@@ -5,6 +8,39 @@
 // [1,2,3,4,5,6] ====> 6
 // [1,1,1,1,1,1,] ====> 1
 // [] ====> 0
+
+const countUnique = (arr) => {
+
+  let left = 0
+  let right = left + 1; 
+  let count = 0
+
+  for (let i = 0; i < arr.length; i++) {
+ 
+    if(arr[left] === arr[right])
+    {
+      right++
+    }
+    else
+    {
+      left = right
+      right++
+      count++
+    }
+    
+  }
+
+  return count
+
+}
+
+// OUTPUT 
+
+console.log(countUnique([1,1,1,1,1,1,2]))
+console.log(countUnique([1,2,3,3,3,4,4,4,4,5,5]))
+console.log(countUnique([-2,-1,-1,0,1]))
+console.log(countUnique([]))
+
 
 // PROBLEM STATEMENT
 //
@@ -32,7 +68,7 @@ const sumZero = (arr) => {
   return [];
 };
 
-console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]));
-console.log(sumZero([-2, -1, 0, 1, 3]));
-console.log(sumZero([1, 2, 3]));
-console.log(sumZero([3]));
+// console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]));
+// console.log(sumZero([-2, -1, 0, 1, 3]));
+// console.log(sumZero([1, 2, 3]));
+// console.log(sumZero([3]));
