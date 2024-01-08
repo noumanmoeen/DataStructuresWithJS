64725a89199ad7723c1968d8805590b0575c6ceb
moeennouman7@gmail.com <moeennouman7@gmail.com>
2024-01-09 00:27:41 +0500

    Solved binary search using recursion

--- Recursion/binarySearch.js
+++ Recursion/binarySearch.js
@@ -3,14 +3,17 @@
 // Write a recursive function to implement binary search
 
 const recursiveBinarySearch = (arr, num) => {
-  let mid = Math.floor(arr[0] + arr[arr.length - 1]) / 2;
+  let mid = Math.floor((0 + (arr.length - 1)) / 2);
+  if (arr.length === 0 && arr[0] !== num) {
+    return false;
+  }
   if (arr[mid] === num) {
     return true;
   } else if (arr[mid] > num) {
-    return recursiveBinarySearch(arr.slice(mid), num);
-  } else {
     return recursiveBinarySearch(arr.slice(0, mid), num);
+  } else {
+    return recursiveBinarySearch(arr.slice(mid + 1), num);
   }
 };
 
-console.log(recursiveBinarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10],2));
+console.log(recursiveBinarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4));
