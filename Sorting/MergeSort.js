5872792ea66821e65c9dbe62b2659cbcfbc9f141
moeennouman7@gmail.com <moeennouman7@gmail.com>
2024-01-21 20:47:16 +0500

    Completed merge sort started working on merge functionality

--- Sorting/MergeSort.js
+++ Sorting/MergeSort.js
@@ -1,5 +1,24 @@
-const mergeSort = () => {
+const merge = (left = [], right = []) => {
+  const result = [];
+  let i = 0,
+    j = 0;
+  while (i <= left.length - 1 && j <= right.length - 1) {
+    if (left[i] <= right[j]) {
+      result.push(left[i]);
+      i++;
+    } else if (left[i] >= right[j]) {
+      result.push(right[j]);
+      j++;
+    }
+  }
+};
 
-}
+const mergeSort = (array = []) => {
+  if (array.length <= 1) return array;
+  const mid = Math.floor((0 + array.length - 1) / 2);
+  const left = mergeSort(array.slice(0, mid));
+  const right = mergeSort(array.slice(mid, array.length - 1));
+  return merge(left, right);
+};
 
-console.log(mergeSort([]))
+console.log(mergeSort([8, 3, 5, 4, 7, 6, 1, 2]));
