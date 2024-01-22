df5c0691c9996bff96fbe449268764742376bc00
moeennouman7@gmail.com <moeennouman7@gmail.com>
2024-01-23 00:43:38 +0500

    Reverting commit

--- Sorting/MergeSort.js
+++ Sorting/MergeSort.js
@@ -3,15 +3,36 @@ const merge = (left = [], right = []) => {
   let i = 0,
     j = 0;
   while (i <= left.length - 1 && j <= right.length - 1) {
-    
+    if (left[i] <= right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
   }
+
+  while (i <= left.length - 1) {
+    result.push(left[i]);
+    i++;
+  }
+
+  while (j <= right.length - 1) {
+    result.push(right[j]);
+    j++;
+  }
+
+  return result;
 };
 
 const mergeSort = (array = []) => {
   if (array.length <= 1) return array;
   const mid = Math.floor((0 + array.length - 1) / 2);
-  const left = mergeSort(array.slice(0, mid));
-  const right = mergeSort(array.slice(mid, array.length - 1));
+
+  const left = mergeSort(array.slice(0, mid + 1));
+
+  const right = mergeSort(array.slice(mid + 1, array.length));
+
   return merge(left, right);
 };
 
