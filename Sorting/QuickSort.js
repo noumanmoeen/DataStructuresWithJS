25ef52ed50341b292c90411ea4fc421e58d3b0e2
moeennouman7@gmail.com <moeennouman7@gmail.com>
2024-01-28 17:41:56 +0500

    Completed pivot helper for quick sort

---
+++ Sorting/QuickSort.js
@@ -0,0 +1,23 @@
+const pivotHelper = (array = []) => {
+  let lessCount = 0;
+  const pivot = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < pivot) {
+      lessCount++;
+      if (i != lessCount) {
+        let temp = array[lessCount];
+
+        array[lessCount] = array[i];
+        array[i] = temp;
+      }
+    }
+  }
+
+  let temp = array[lessCount];
+  array[lessCount] = array[0];
+  array[0] = temp;
+  let ttemp = array;
+  return { lessCount, array };
+};
+
+console.log(pivotHelper([5, 2, 1, 8, 4, 7, 6, 3]));
