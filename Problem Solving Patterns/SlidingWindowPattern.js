8a12065884d4d216804b5814743061af3472ac74
moeennouman7@gmail.com <moeennouman7@gmail.com>
2025-01-31 21:50:00 +0500

    Added longest substring finder

--- Problem Solving Patterns/SlidingWindowPattern.js
+++ Problem Solving Patterns/SlidingWindowPattern.js
@@ -27,4 +27,24 @@ console.log(temp)
 // console.log(maxSubArray([1, 2, 5, 2, 8, 1, 5], 4));
 
 
- 
+function lengthOfLongestSubstring(s) {
+  let charSet = new Set();
+  let left = 0;
+  let maxLength = 0;
+
+  for (let right = 0; right < s.length; right++) {
+      while (charSet.has(s[right])) {
+          charSet.delete(s[left]);
+          left++;
+      }
+      charSet.add(s[right]);
+      maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+}
+
+// Example usage
+console.log(lengthOfLongestSubstring("abcabcbb")); // Output: 3
+console.log(lengthOfLongestSubstring("bbbbb"));    // Output: 1
+console.log(lengthOfLongestSubstring("pwwkew"));   // Output: 3
